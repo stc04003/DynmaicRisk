8c015af47338d034bac3f61836f806f2c4d58f01
Steven Chiou <schiou@utdallas.edu>
2022-08-07 19:18:50 -0500

    adding variable importance

--- codes/run.R
+++ codes/run.R
@@ -1,5 +1,6 @@
 ## Load function for simulating data
-source("data.R")
+## sim3A.R, sim3B.R, or sim3C.R
+source("sim3C.R")
 
 ## Load function for ensemble 
 source("ranger-addon.R")
@@ -8,17 +9,67 @@ source("ranger-addon.R")
 library(ranger)
 library(survival)
 
-## Generate training data and fitting from scenario 3C
+## Generate training data from scenario 3C
 dat <- simDat3C(400, .2)
+
+## Fits ranger
 fit <- ranger(Surv(Time, status) ~ ., data = dat)
 
 ## Generate testing data and computes the predicted probabilities
-dat0 <- simDat(500, 0)
+dat0 <- simDat3C(500, 0)
 predS <- getSurv(fit, dat, dat0)
 trueS <- getTrueSurv3C(dat0)
-  
+
 t0 <- seq(0, quantile(dat0$Time, .9), .01)
 ## Intergrated absolute error
 mean(sapply(1:nrow(dat0), function(i) abs(predS[[i]](t0) - trueS[[i]](t0))))
 ## Intergrated MSE
 mean(sapply(1:nrow(dat0), function(i) (predS[[i]](t0) - trueS[[i]](t0))^2))
+
+
+## Variable importance
+
+vnames <- setdiff(names(dat), c("Time", "status", "D"))
+p <- length(vnames)
+
+getVi <- function(fit, df) {
+  df <- df[order(df$Time + df$status),]
+  tt <- sort(df$Time)
+  rownames(df) <- NULL
+  sapply(1:nrow(df), function(s) {
+    fit.tmp <- fit
+    takei <- which(unlist(lapply(fit$forest$OobSampleIDs, function(x) any(x %in% (s - 1)))))
+    fit.tmp$forest$OobSampleIDs <- fit.tmp$forest$OobSampleIDs[takei]
+    fit.tmp$forest$num.trees <- fit.tmp$num.trees <- length(takei)
+    fit.tmp$forest$child.nodeIDs <- fit.tmp$forest$child.nodeIDs[takei]
+    fit.tmp$forest$split.values <- fit.tmp$forest$split.values[takei]
+    fit.tmp$forest$SampleIDs <- fit.tmp$forest$SampleIDs[takei]
+    fit.tmp$forest$chf <- fit.tmp$forest$chf[takei]
+    fit.tmp$forest$split.varIDs <- fit.tmp$forest$split.varIDs[takei]
+    getSurv(fit.tmp, df, df[s,])
+  })
+}
+
+
+getCON <- function(tt, si, df0) {
+  c1 <- outer(df0$status, rep(1, length(df0$status)))
+  c2 <- outer(df0$Time <= tt, df0$Time > tt)
+  l <- length(si)
+  stmp <- sapply(1:l, function(x) si[[x]](tt))
+  c3 <- 1 * outer(stmp, stmp, "<") + 0.5 * outer(stmp, stmp, "==")
+  ## c4 <- outer(sc(df0$Time + df0$D), sc(df0$Time + tt))
+  ## c4 <- ifelse(c4 < 1e-5, NA, c4)
+  c4 <- 1
+  sum(c1 * c2 * c3 / c4, na.rm = TRUE) / sum(c1 * c2 / c4, na.rm = TRUE)
+}
+
+
+i <- 2
+B <- 10
+
+replicate(B, {
+  dat2 <- dat[order(dat$Time),]
+  dat2[,vnames[i]] <- sample(dat2[,vnames[i]])
+  f <- getVi(fit, dat2)
+  mean(sapply(t0, getCON, f, dat2), na.rm = T)})
+
